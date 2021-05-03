a43a3b3c13d8e0ac51f400f64f80cd763f01a6e5
Thomas Semiz <tas56@njit.edu>
2021-05-03 17:31:57 -0400

    Set up database

---
+++ db/init.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE banking;
+use banking;
+
+CREATE TABLE IF NOT EXISTS accounts (
+    `id` int AUTO_INCREMENT,
+    `username` VARCHAR(21) CHARACTER SET utf8,
+    `balance` NUMERIC(19, 2),
+    PRIMARY KEY (`id`)
+    );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    `id` int AUTO_INCREMENT,
+    `account_id` INT,
+    `amount` NUMERIC(19, 4),
+    `transaction_date` CURRENT_DATE(),
+    `transaction_type` VARCHAR(21) CHARACTER SET utf8,
+    PRIMARY KEY (`id`)
+    );
+
+INSERT INTO accounts (username,balance) VALUES
+    ('Thomas Semiz', 900.00),
+    ('John Smith', 375.00)
+
+INSERT INTO transactions (account_id,amount,transaction_date,transaction_type) VALUES
+    (1, 900.00, null, 'deposit'),
+    (2, 375.00, null, 'deposit')
