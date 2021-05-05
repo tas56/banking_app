4e8132df608916cd7600f49729dcff8029489484
Thomas Semiz <tas56@njit.edu>
2021-05-05 10:40:00 -0400

    Added Auth0 to index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './components/App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ReactDOM.render(
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin} >
+        <App />
+    </Auth0Provider>,
+    document.getElementById('root')
+);
