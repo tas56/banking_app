76b8e5ef201d06e603da2e31c733ebbeec4cfbd3
Thomas Semiz <tas56@njit.edu>
2021-05-11 00:30:03 -0400

    UserAccount view

--- src/views/UserAccount.js
+++ src/views/UserAccount.js
@@ -1,7 +1,8 @@
 import {  useParams } from "react-router-dom";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import axios from "axios";
+
 import { fetchAccounts } from "../actions/accountActions";
 import { fetchTransactions } from "../actions/transactionsActions";
 
@@ -13,17 +14,35 @@ const UserAccount = (props) => {
 
 
     useEffect( () => {
-        props.fetchAccounts()
-        props.fetchTransactions()
-    });
 
-    console.log(props.accounts)
+        axios.get('http://localhost:8000/api/v1/accounts')
+            .then(res => {
+                let data = res.data;
+                setAccounts(data);
+            }).catch(error => {
+            console.log(error);
+        })
+
+    }, [] );
+
+    console.log(accounts)
 
 
-   const userAccount = props.accounts.find(account => account.id === parseInt(id));
+   const userAccount = accounts.find(account => account.id === parseInt(id));
     console.log(userAccount)
 
-    const userTransactions = props.transactions.map(transaction => transaction.account_id === parseInt(id));
+    const userTransactions = props.transactions.map(transaction => transaction.account_id === parseInt(id))
+        .map(transaction => {
+            return (
+                <tr key={transaction.id}>
+                    <th scope="row">{transaction.id}</th>
+                    <td>{parseFloat(transaction.amount).toFixed(2)}</td>
+                    <td>{transaction.account_id}</td>
+                    <td>{transaction.transaction_type}</td>
+                    <td>{transaction.transaction_date}</td>
+                </tr>
+            )
+        })
     console.log(userTransactions);
 
     return (
@@ -34,6 +53,20 @@ const UserAccount = (props) => {
             <h3>Account Balance:</h3>
             <h2>${userAccount.balance}</h2>
             <h3>Transactions</h3>
+            <table className="table table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Account ID</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                {userTransactions}
+                </tbody>
+            </table>
 
         </div>
     )
